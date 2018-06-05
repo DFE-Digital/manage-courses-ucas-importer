490b4baad724934d137a5dfd4eb46b267831e4f5
Tim Abell <tim@timwise.co.uk>
2018-06-05 16:29:47 +0100

    List xls files in the folder

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Microsoft.Extensions.CommandLineUtils;
 
 namespace UcasCourseImporter
@@ -8,11 +9,17 @@ namespace UcasCourseImporter
         static void Main(string[] args)
         {
             var app = new CommandLineApplication();
-            var folder = app.Option("-$|-f|--folder <folder>", "Folder to read UCAS .xls files from ", CommandOptionType.SingleValue);
+            var folderOption = app.Option("-$|-f|--folder <folder>", "Folder to read UCAS .xls files from ", CommandOptionType.SingleValue);
             app.HelpOption("-?|-h|--help");
             app.Execute(args);
             Console.Write("Reading xls files from: ");
-            Console.WriteLine(folder.Value());
+            Console.WriteLine(folderOption.Value());
+
+            var folder = new DirectoryInfo(folderOption.Value());
+            foreach (var file in folder.GetFiles("*.xls"))
+            {
+                Console.WriteLine(" - " + file.FullName);
+            }
         }
     }
 }
