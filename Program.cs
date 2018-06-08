f10465e9c0c34a2d3e4705427d0ddcfad3ce2a17
Tim Abell <tim@timwise.co.uk>
2018-06-08 18:52:34 +0100

    Stub code for posting courses to the manage courses api
    I think it makes sense to source-control the json so it's
    easy to see how the api has changed, and then have the generated
    c# client code excluded from source-control.
    
    Using before-compile because before-build didn't work if build was already broken:
    https://stackoverflow.com/questions/27986147/target-not-running-when-using-beforetargets-build-on-build-server
    
    * Factor out file reader code

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using Microsoft.Extensions.CommandLineUtils;
 using NPOI.HSSF.UserModel;
 
-namespace UcasCourseImporter
+namespace GovUk.Education.ManageCourses.UcasCourseImporter
 {
     class Program
     {
@@ -14,12 +16,37 @@ namespace UcasCourseImporter
             var folderOption = app.Option("-$|-f|--folder <folder>", "Folder to read UCAS .xls files from ", CommandOptionType.SingleValue);
             app.HelpOption("-?|-h|--help");
             app.Execute(args);
-            Console.Write("Reading xls files from: ");
-            Console.WriteLine(folderOption.Value());
 
-            var folder = new DirectoryInfo(folderOption.Value());
+            ReadFiles(folderOption.Value());
+            HitApi();
+        }
+
+        private static void HitApi()
+        {
+            Console.WriteLine("Posting to api...");
+            var client = new ManageCoursesApiClient();
+            var payload = new Payload
+            {
+                Courses = new ObservableCollection<Course>
+                {
+                    new Course
+                    {
+                        Title = "Defence Against The Dark Arts",
+                        CourseCode = "HP1",
+                        NctlId = "123",
+                    }
+                }
+            };
+            client.ImportAsync(payload).Wait();
+            Console.WriteLine("Done.");
+        }
+
+        private static void ReadFiles(string folder)
+        {
+            Console.Write("Reading xls files from: ");
+            Console.WriteLine(folder);
             int totalrowcount = 0;
-            foreach (var file in folder.GetFiles("*.xls"))
+            foreach (var file in new DirectoryInfo(folder).GetFiles("*.xls"))
             {
                 Console.WriteLine(" - " + file.FullName);
                 using (var stream = new FileStream(file.FullName, FileMode.Open))
@@ -29,7 +56,7 @@ namespace UcasCourseImporter
                     Console.WriteLine(" -- " + sheet.SheetName);
                     var header = sheet.GetRow(0);
                     Console.WriteLine(" --- Cols: "
-                          + string.Join(", ", header.Cells.Select(c => c.StringCellValue)));
+                                      + string.Join(", ", header.Cells.Select(c => c.StringCellValue)));
                     int rowcount = 0;
                     foreach (var row in sheet)
                     {
@@ -41,6 +68,7 @@ namespace UcasCourseImporter
                     totalrowcount += rowcount;
                 }
             }
+
             Console.WriteLine("total rowcount: " + totalrowcount);
         }
     }
