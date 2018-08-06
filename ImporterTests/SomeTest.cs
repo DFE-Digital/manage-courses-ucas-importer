424b5b71489d9c103fcf0bf384fdd2bc108dd4be
Tim Abell <tim@timwise.co.uk>
2018-08-06 11:01:10 +0100

    Add stub tests
    To test CI is setup correctly

---
+++ ImporterTests/SomeTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace ImporterTests
+{
+    [TestFixture]
+    public class SomeTest
+    {
+        [Test]
+        public void AlwaysGreen()
+        {
+            // todo: test something meaningful.
+            // this is just here to show that the CI is working.
+            Assert.True(true);
+        }
+
+    }
+}
