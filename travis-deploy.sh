3790f25721ee6b78d407b539c17778d9a2984412
Tim Abell <tim@timwise.co.uk>
2018-08-15 17:06:16 +0100

    Iterating publish curl

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -6,4 +6,4 @@ dotnet publish src/importer --configuration Release
 
 zip $deployZip -r src/importer/bin/Release/netcoreapp2.0/publish
 
-curl -X PUT -u "govuk-dfe-bat-deployment-user:$AZURE_WA_PASSWORD" --data-binary $deployZip https://bat-dev-manage-courses-api-app.scm.azurewebsites.net/api/triggered/ucas-import
+curl -X PUT -u "$userName:$userPWD" --data-binary $deployZip https://bat-dev-manage-courses-api-app.scm.azurewebsites.net/api/triggered/ucas-import
