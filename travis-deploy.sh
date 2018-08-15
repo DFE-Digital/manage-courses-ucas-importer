3b5737ac791ba8c252efd962b0f478d8b2c5683b
the-daniel-rothig <daniel.rothig@gmail.com>
2018-08-15 21:05:44 +0100

    experiment 3... of 2

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -7,4 +7,4 @@ dotnet publish src/importer --configuration Release
 zip $deployZip -r src/importer/bin/Release/netcoreapp2.0/publish
 
 curl -X DELETE -u "$1" https://bat-dev-manage-courses-api-app.scm.azurewebsites.net/api/triggeredwebjobs/ucas-import
-curl -X PUT -u "$1" --data-binary $deployZip --header "Content-Type: application/zip" --header "Content-Disposition: attachment; filename=deploy.zip" https://bat-dev-manage-courses-api-app.scm.azurewebsites.net/api/zip/site/wwwroot/App_Data/jobs/triggered/ucas-import/
+curl -X PUT -u "$1" --data $deployZip --header "Content-Type: application/zip" --header "Content-Disposition: attachment; filename=deploy.zip" https://bat-dev-manage-courses-api-app.scm.azurewebsites.net/api/zip/site/wwwroot/App_Data/jobs/triggered/ucas-import/
