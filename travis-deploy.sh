ed797e90b56b58c038b60e3ae8eefbdf20e06388
Tim Abell <tim@timwise.co.uk>
2018-08-15 16:43:34 +0100

    Actual url for deploy

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 dotnet publish src/importer --configuration Release
 zip deploy.zip -r src/importer/bin/Release/netcoreapp2.0/publish
-curl -X PUT --data deploy.zip --header "Content-Disposition: attachment; attachment; filename=Copy.zip" --header "Authorization: " https://b.scm.azurewebsites.net/api/triggered/ucas-import
+curl -X PUT --data deploy.zip --header "Content-Disposition: attachment; attachment; filename=Copy.zip" --header "Authorization: " https://bat-dev-manage-courses-api-app.scm.azurewebsites.net/api/triggered/ucas-import
