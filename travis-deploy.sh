218465ba09d8eb02a0246fa9566c6e44d9723d0a
the-daniel-rothig <daniel.rothig@gmail.com>
2018-08-15 17:27:19 +0100

    correct api endpoint in KUDU

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -6,4 +6,4 @@ dotnet publish src/importer --configuration Release
 
 zip $deployZip -r src/importer/bin/Release/netcoreapp2.0/publish
 
-curl -X PUT -u "$1" --data-binary $deployZip https://bat-dev-manage-courses-api-app.scm.azurewebsites.net/api/triggered/ucas-import
+curl -X PUT -u "$1" --data-binary $deployZip https://bat-dev-manage-courses-api-app.scm.azurewebsites.net/api/triggeredwebjobs/ucas-import
