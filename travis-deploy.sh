f9e677a3e886e8cd750e450a3e5bad895e7c88ce
the-daniel-rothig <daniel.rothig@gmail.com>
2018-08-15 17:32:39 +0100

    add missing headers

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -6,4 +6,4 @@ dotnet publish src/importer --configuration Release
 
 zip $deployZip -r src/importer/bin/Release/netcoreapp2.0/publish
 
-curl -X PUT -u "$1" --data-binary $deployZip https://bat-dev-manage-courses-api-app.scm.azurewebsites.net/api/triggeredwebjobs/ucas-import
+curl -X PUT -u "$1" --data-binary $deployZip --header "Content-Type: application/zip" --header "Content-Disposition: attachment; filename=$deployZip" https://bat-dev-manage-courses-api-app.scm.azurewebsites.net/api/triggeredwebjobs/ucas-import
