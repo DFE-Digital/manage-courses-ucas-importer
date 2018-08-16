dbf8cebd044440628a97afb5873b126d6d1f9065
the-daniel-rothig <daniel.rothig@gmail.com>
2018-08-16 15:09:35 +0100

    branchwise deploy

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -14,6 +14,6 @@ zip $deployZip -r src/importer/bin/Release/netcoreapp2.0/publish
 echo "deleting old azure WebJob..."
 curl -X DELETE -u "$1" https://bat-dev-manage-courses-api-app.scm.azurewebsites.net/api/triggeredwebjobs/ucas-import
 echo "uploading to azure WebJob..."
-curl -X PUT -u "$1" --data-binary @$deployZip --header "Content-Type: application/zip" --header "Content-Disposition: attachment; filename=$deployZip" https://bat-dev-manage-courses-api-app.scm.azurewebsites.net/api/triggeredwebjobs/ucas-import/
+curl -X PUT -u "$1" --data-binary @$deployZip --header "Content-Type: application/zip" --header "Content-Disposition: attachment; filename=$deployZip" https://$2.scm.azurewebsites.net/api/triggeredwebjobs/ucas-import/
 
 echo "Deploy complete."
