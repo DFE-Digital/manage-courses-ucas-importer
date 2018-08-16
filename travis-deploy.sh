c0ed6d8de2ca0201c7f82c56333a035236724ef0
Tim Abell <tim@timwise.co.uk>
2018-08-16 09:49:45 +0100

    Add some progress echos

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -2,11 +2,18 @@
 
 deployZip=deploy.zip
 
+echo "Removing old $deployZip if any..."
 [ -e $deployZip ] && rm $deployZip
 
+echo "running dotnet publish..."
 dotnet publish src/importer --configuration Release
 
+echo "creating zip..."
 zip $deployZip -r src/importer/bin/Release/netcoreapp2.0/publish
 
+echo "deleting old azure WebJob..."
 curl -X DELETE -u "$1" https://bat-dev-manage-courses-api-app.scm.azurewebsites.net/api/triggeredwebjobs/ucas-import
+echo "uploading to azure WebJob..."
 curl -X PUT -u "$1" --data-binary @$deployZip --header "Content-Type: application/zip" --header "Content-Disposition: attachment; filename=$deployZip" https://bat-dev-manage-courses-api-app.scm.azurewebsites.net/api/triggeredwebjobs/ucas-import/
+
+echo "Deploy complete."
