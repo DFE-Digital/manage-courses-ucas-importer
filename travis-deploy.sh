dbc6e28d4c1bebc14d375173efbb32dfe2ebe8ca
the-daniel-rothig <daniel.rothig@gmail.com>
2018-08-21 14:53:07 +0100

    hotfix: properly parametrise delete

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -12,7 +12,7 @@ echo "creating zip..."
 zip $deployZip -r src/importer/bin/Release/netcoreapp2.0/publish
 
 echo "deleting old azure WebJob..."
-curl -X DELETE -u "$1" https://bat-dev-manage-courses-api-app.scm.azurewebsites.net/api/triggeredwebjobs/ucas-import
+curl -X DELETE -u "$1" https://$2.scm.azurewebsites.net/api/triggeredwebjobs/ucas-import
 echo "uploading to azure WebJob..."
 curl -X PUT -u "$1" --data-binary @$deployZip --header "Content-Type: application/zip" --header "Content-Disposition: attachment; filename=$deployZip" https://$2.scm.azurewebsites.net/api/triggeredwebjobs/ucas-import/
 
